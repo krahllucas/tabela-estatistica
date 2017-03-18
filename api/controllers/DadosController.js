b1685551574b1581001caae350945c63d1c03dcb
Lucas Krahl <luckrahl1996@gmail.com>
2017-03-18 02:57:35 -0300

    cadastro de dados

--- api/controllers/DadosController.js
+++ api/controllers/DadosController.js
@@ -6,6 +6,39 @@
  */
 
 module.exports = {
-	
+	tabela: function(req, res, next){
+		Tabela.findOne(req.param('id')).exec(function tabelaFounded(err, tabela){
+			Dados.find({tabela: req.param('id')}).sort('valor').exec(function dadosFounded(err, dados){
+				return res.view({ dados: dados, tabela: tabela });
+			});
+		});
+	},
+	create:function(req, res){
+		var dadoObj={
+			valor: req.param('valor'),
+			tabela: req.param('tabela')
+		};
+
+		Dados.create(dadoObj, function(err, dado){
+			return res.redirect('/dados/tabela/'+req.param('tabela'));
+		});
+	},
+	update:function(req, res, next){
+		var dadoObj={
+			valor: req.param('valor'),
+			tabela: req.param('tabela')
+		};
+
+		Dados.update(req.param('id'),dadoObj, function dadoUpdated(err, dado){
+			return res.redirect('/dados/tabela/'+req.param('tabela'));
+		});
+	},
+	destroy:function(req,res){
+		Dados.findOne(req.param('id')).populateAll().exec(function dadoFounded(err, dad){
+			Dados.destroy(req.param('id'), function dadosDestroy(err, dado){
+				return res.redirect('/dados/tabela/'+dad.tabela.id);
+			});
+		});
+	}
 };
 
