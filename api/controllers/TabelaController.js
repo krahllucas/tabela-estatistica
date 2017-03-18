98e5fb2001df5096f9136850d9e003cd105d8e09
Lucas Krahl <luckrahl1996@gmail.com>
2017-03-18 02:20:17 -0300

    cadastro de tabelas

--- api/controllers/TabelaController.js
+++ api/controllers/TabelaController.js
@@ -23,6 +23,20 @@ module.exports = {
 		Tabela.create(tabelaObj, function(err, tabela){
 			return res.redirect('/tabela');
 		});
+	},
+	update:function(req, res, next){
+		var tabelaObj={
+			nome: req.param('nome')
+		};
+
+		Tabela.update(req.param('id'),tabelaObj, function tabelaUpdated(err, tabela){
+			return res.redirect('/tabela');
+		});
+	},
+	destroy:function(req,res){
+		Tabela.destroy(req.param('id'), function tabelaDestroy(err, tabela){
+			return res.redirect('/tabela');
+		});
 	}
 };
 
